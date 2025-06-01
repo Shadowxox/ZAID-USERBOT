11e3e70db0a9359a43c0afcc98fe1771776bdfcc
Shadowxox <shadowx0x12@gmail.com>
2025-06-01 14:54:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d") #optional
+API_ID = int(getenv("API_ID", "23287799")) #optional
+API_HASH = getenv("API_HASH", "9f4f17dae2181ee22c275b9b40a3c907") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7951600439").split()))
+OWNER_ID = int(getenv("OWNER_ID", "7795212861"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://dbzcommunity7:8aaNMFXxpvJceE3h@cluster0.bglvihu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = getenv("BOT_TOKEN", "8045371426:AAGjC5d5APxZJdAkGoue33tBir2IP91Q1fI")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/lchqg1.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
+LOG_GROUP = getenv("LOG_GROUP", "DBZ_LOG_GC")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+REPO_URL = getenv("REPO_URL", "https://t.me/DBZ_ONGOING")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
